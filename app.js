36ec6b97daedabf99a1e0f2f67be1b9bc4e75880
DecioFrancis <francisdasilva@gmail.com>
2024-12-18 14:19:22 -0300

    feat(route): creats route jobs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const app = express();
-const db = require('./db/connection');
+const app     = express();
+const db      = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
   console.log(`Express is running at the door ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false}));
 
 // db connection
 db
@@ -22,3 +26,6 @@ db
 app.get('/', (req, res) => {
   res.send("It's working");
 })
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
